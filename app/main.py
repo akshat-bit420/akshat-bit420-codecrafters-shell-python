e33ab2f71ded472678bc7f77833b3bedd829bc7e
akshat-bit420 <as.93688akshat@gmail.com>
2026-07-08 15:06:36 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,25 +1,63 @@
+import os
 import sys
-import shutil
+import subprocess
 
-def main():
+VALID_COMMANDS = set({"exit", "echo", "type"})
+
+
+def find_executable(command: str) -> str | None:
+    path_env = os.environ.get("PATH")
+    if not path_env:
+        return None
+
+    for directory in path_env.split(os.pathsep):
+        candidate = os.path.join(directory, command)
+        if os.path.isfile(candidate) and os.access(candidate, os.X_OK):
+            return candidate
+
+    return None
+
+
+def main() -> None:
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command == "exit":
-            break
-        elif command.startswith("echo "):
-            print(command[5:])
-        elif command.startswith("type "):
-            subject = command[5:]
-            if subject in ["echo", "exit", "type"]:
-                print(f"{subject} is a shell builtin")
-            elif path := shutil.which(subject):
-                print(f"{subject} is {path}")
-            else:
-                print(f"{subject}: not found")
-        else:
-            print(f"{command}: command not found")
+        line = input()
 
+        if not line.strip():
+            continue
+
+        parts = line.split()
+        cmd = parts[0]
+
+        if cmd == "exit":
+            sys.exit(0)
+
+        elif cmd == "echo":
+            print(" ".join(parts[1:]))
+
+        elif cmd == "type":
+            if len(parts) < 2:
+                continue
+
+            target = parts[1]
+            if target in VALID_COMMANDS:
+                print(f"{target} is a shell builtin")
+            else:
+                found = find_executable(target)
+                if found:
+                    print(f"{target} is {found}")
+                else:
+                    print(f"{target}: not found")
+
+        else:
+            found = find_executable(cmd)
+            if not found:
+                print(f"{line}: command not found")
+            else:
+                res = subprocess.run(args=parts)
+                sys.exit(res.returncode)
+    else:
+        res = subprocess.run(args=parts)
 
 
 if __name__ == "__main__":
